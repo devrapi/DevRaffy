e21047ea7a079bf0be925dd02bc709e5c025bfb4
unknown <raffymaranan22@gmail.com>
2025-01-27 16:50:40 +0800

    changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Project1 from "./project1";
+import Project1 from "./Project1";
 import Project2 from "./Project2";
 import Project3 from "./Project3";
 const Projects = () => {
